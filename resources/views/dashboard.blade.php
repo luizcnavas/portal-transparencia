6e35ea19916110bc498fe7fce9277fcf69689bbc
GabiDourado <gabi.dourados2xd@gmail.com>
2025-11-15 10:29:17 -0300

    Erro do dashboard corrigido

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,34 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h1 class="mb-4">Dashboard</h1>
-
-    <div class="row">
-        <div class="col-md-4">
-            <div class="card text-white bg-danger mb-3 fundoDesp">
-                <div class="card-header">Total de Despesas</div>
-                <div class="card-body">
-                    <h5 class="card-title">R$ {{ number_format($totalDespesas, 2, ',', '.') }}</h5>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card text-white bg-success mb-3 fundoRec">
-                <div class="card-header">Total de Receitas</div>
-                <div class="card-body">
-                    <h5 class="card-title">R$ {{ number_format($totalReceitas, 2, ',', '.') }}</h5>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card text-white {{ $balanco >= 0 ? 'bg-primary' : 'bg-warning' }} mb-3 fundoSaldo">
-                <div class="card-header">Balanço Final</div>
-                <div class="card-body">
-                    <h5 class="card-title">R$ {{ number_format($balanco, 2, ',', '.') }}</h5>
-                </div>
-            </div>
-        </div>
+<div class="container"> 
+    <h1 class="mb-4">Dashboard</h1> 
+    <div class="row"> 
+        <div class="col-md-4"> 
+            <div class="card text-white bg-danger mb-3 fundoDesp"> 
+                <div class="card-header">Total de Despesas</div> 
+                <div class="card-body"> 
+                    <h5 class="card-title">R$ {{ number_format($totalDespesas, 2, ',', '.') }}</h5> 
+                </div> 
+            </div> 
+        </div> 
+        <div class="col-md-4"> 
+            <div class="card text-white bg-success mb-3 fundoRec"> 
+                <div class="card-header">Total de Receitas</div> 
+                <div class="card-body"> 
+                    <h5 class="card-title">R$ {{ number_format($totalReceitas, 2, ',', '.') }}</h5> 
+                </div> 
+            </div> 
+        </div> 
+        <div class="col-md-4"> 
+            <div class="card text-white {{ $balanco >= 0 ? 'bg-primary' : 'bg-warning' }} mb-3 fundoSaldo"> 
+                <div class="card-header">Balanço Final</div> 
+                <div class="card-body"> 
+                    <h5 class="card-title">R$ {{ number_format($balanco, 2, ',', '.') }}</h5> 
+                </div> 
+            </div> 
+        </div> 
     </div>
 
     {{-- Placeholder for charts --}}
