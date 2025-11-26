dd454ffccd3a0346a751e9f70305b65476050074
GabiDourado <gabi.dourados2xd@gmail.com>
2025-11-25 22:13:06 -0300

    Adição e continuação do layout da tela de notícias

--- resources/views/noticias/index.blade.php
+++ resources/views/noticias/index.blade.php
@@ -10,21 +10,23 @@
 
     <div class="row">
         @forelse($noticias as $noticia)
-            <div class="@if($loop->first) col-md-12 destaque @else col-md-4 @endif mb-4">
-                <div class="card">
+            <div class="@if($loop->first) col-md-8 destaque @else col-md-4 @endif mb-4">
+                <div class="card cardNoticia">
                     <div class="card-body">
-                        <h2 class="card-title">{{ $noticia->titulo }}</h2>
-                        <p class="card-text"><small class="text-muted">Publicado em {{ $noticia->created_at->format('d/m/Y') }}</small></p>
+                        <h2 class="card-title tituloNoticia">{{ $noticia->titulo }}</h2>
+                        <p class="card-text"><small class="text-muted dataNoticia">Publicado em {{ $noticia->created_at->format('d/m/Y') }}</small></p>
                         <p class="card-text">{{ Str::limit($noticia->conteudo, 200) }}</p>
-                        <a href="{{ route('noticias.show', $noticia) }}" class="btn btn-secondary">Leia mais</a>
-                         @auth
-                            <a href="{{ route('admin.noticias.edit', $noticia) }}" class="btn btn-sm btn-warning">Editar</a>
-                            <form action="{{ route('admin.noticias.destroy', $noticia) }}" method="POST" class="d-inline" onsubmit="return confirm('Tem certeza que deseja excluir?');">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-sm btn-danger">Excluir</button>
-                            </form>
-                        @endauth
+                        <div class="btnNoticia">
+                            <a href="{{ route('noticias.show', $noticia) }}" class="btn lerMais">Leia mais</a>
+                            @auth
+                                <a href="{{ route('admin.noticias.edit', $noticia) }}" class="btn btn-sm botaoTab"><img class="imgBtn" src="{{ asset('assets/img/botoes/paraTabelas/botao-editar.png') }}" alt="Editar"/></a>
+                                <form action="{{ route('admin.noticias.destroy', $noticia) }}" method="POST" class="d-inline" onsubmit="return confirm('Tem certeza que deseja excluir?');">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-sm botaoTab"><img class="imgBtn" src="{{ asset('assets/img/botoes/paraTabelas/excluir.png') }}" alt="Excluir"/></button>
+                                </form>
+                            @endauth
+                        </div>
                     </div>
                 </div>
             </div>
